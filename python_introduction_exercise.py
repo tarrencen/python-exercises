87dbb65d5468578ec05ec1f1f784e16045a2c4a9
tarrencen <tarrence.nichols@gmail.com>
2022-01-18 13:25:37 -0600

    Edited greeting

--- python_introduction_exercise.py
+++ python_introduction_exercise.py
@@ -1,7 +1,4 @@
 print ('Hello World!')
 greeting = 'Welcome back, snek charmer!!'
 print (greeting)
-import numpy as np
-fibonacci = np.array(2, 3, 5, 8, 13, 21, 34, 55, 89)
-print (fibonacci)
-print (greeting)
+
